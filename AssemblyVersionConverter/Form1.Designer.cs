9d0e9314b128aaf90a5e6e641212ef5f7536c4f9
Alex Virlan <dj.3agl3@gmail.com>
2024-01-27 15:11:56 +0200

    Initial commit with just the POC version of the future app

---
+++ AssemblyVersionConverter/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace AssemblyVersionConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBuild = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRevision = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(9, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X.X.";
+            // 
+            // txtBuild
+            // 
+            this.txtBuild.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(46)))), ((int)(((byte)(46)))));
+            this.txtBuild.ForeColor = System.Drawing.Color.White;
+            this.txtBuild.Location = new System.Drawing.Point(42, 8);
+            this.txtBuild.Name = "txtBuild";
+            this.txtBuild.Size = new System.Drawing.Size(95, 20);
+            this.txtBuild.TabIndex = 1;
+            this.txtBuild.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(141, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = ".";
+            // 
+            // txtRevision
+            // 
+            this.txtRevision.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(46)))), ((int)(((byte)(46)))));
+            this.txtRevision.ForeColor = System.Drawing.Color.White;
+            this.txtRevision.Location = new System.Drawing.Point(155, 8);
+            this.txtRevision.Name = "txtRevision";
+            this.txtRevision.Size = new System.Drawing.Size(95, 20);
+            this.txtRevision.TabIndex = 2;
+            this.txtRevision.Text = "0";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(256, 6);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(53, 23);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(46)))), ((int)(((byte)(46)))));
+            this.txtResult.ForeColor = System.Drawing.Color.White;
+            this.txtResult.Location = new System.Drawing.Point(12, 34);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(297, 20);
+            this.txtResult.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(321, 62);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtRevision);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBuild);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Assembly Version Converter - AvA.Soft";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBuild;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRevision;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ AssemblyVersionConverter/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AssemblyVersionConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            DateTime date = new DateTime(2000, 1, 1)                    // baseline is 01/01/2000
+                .AddDays(Convert.ToDouble(txtBuild.Text))               // build is number of days after baseline
+                .AddSeconds(Convert.ToDouble(txtRevision.Text) * 2);    // revision is half the number of seconds into the day
+
+            txtResult.Text = $"{date:dddd, dd.MM.yyyy} - {date:HH:mm:ss}";
+        }
+    }
+}
